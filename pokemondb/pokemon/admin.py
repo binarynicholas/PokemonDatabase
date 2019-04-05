95260935e3332ecb237e156cbcb9f3466f5f8d9c
Nicholas Mulrooney <mulrnk1@student.op.ac.nz>
2019-04-05 17:22:44 +1300

    Implemented two models (Species and Ability) and worked on their database presentation via the admin site.

--- pokemondb/pokemon/admin.py
+++ pokemondb/pokemon/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import Species, Ability
+
 # Register your models here.
+
+admin.site.register(Ability)
+
+class AbilityInLine(admin.TabularInline):
+    model=Ability
+
+class SpeciesAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': [
+            'species_name', 'type_primary', 'type_secondary',
+            'ability_1', 'ability_2', 'hidden_ability',
+            'egg_group_1', 'egg_group_2'
+        ]}),
+        ('Biometric information', {'fields': [
+            'min_height', 'max_height',
+            'min_weight', 'max_weight'
+        ]}),
+        ('Metadata', {'fields': ['date_added']}),
+    ]
+    list_display = ('species_name', 'type', 'ability_1', 'ability_2', 'hidden_ability')
+
+
+admin.site.register(Species, SpeciesAdmin)
--- pokemondb/pokemon/models.py
+++ pokemondb/pokemon/models.py
@@ -1,3 +1,127 @@
 from django.db import models
 
 # Create your models here.
+
+class Species(models.Model):
+    TYPES = (('0', 'Normal'), 
+        ('1', 'Fire'), 
+        ('2', 'Fighting'), 
+        ('3', 'Water'), 
+        ('4', 'Grass'), 
+        ('5', 'Flying'), 
+        ('6', 'Poison'), 
+        ('7', 'Electric'), 
+        ('8', 'Ground'), 
+        ('9', 'Psychic'), 
+        ('10', 'Rock'), 
+        ('11', 'Ice'), 
+        ('12', 'Bug'), 
+        ('13', 'Dragon'), 
+        ('14', 'Ghost'), 
+        ('15', 'Dark'), 
+        ('16', 'Steel'), 
+        ('17', 'Fairy'), 
+        ('18', 'Cyber'),
+    )
+    EGG_GROUPS = (('0', 'Monster'),
+        ('1', 'Human-Like'),
+        ('2', 'Bug'),
+        ('3', 'Water 1'),
+        ('4', 'Water 2'),
+        ('5', 'Water 3'),
+        ('6', 'Mineral'),
+        ('7', 'Flying'),
+        ('8', 'Amorphous'),
+        ('9', 'Field'),
+        ('10', 'Fairy'),
+        ('11', 'Ditto'),
+        ('12', 'Grass'),
+        ('13', 'Dragon'),
+        ('14', 'Undiscovered'),
+        ('15', 'Gender unknown'),
+    )
+    species_name = models.CharField(max_length=30)
+    date_added = models.DateTimeField('Date Added')
+    type_primary = models.CharField(
+        max_length=10,
+        choices=TYPES,
+        default='Normal')
+    type_secondary = models.CharField(
+        max_length=10,
+        choices=TYPES,
+        blank=True,
+        null=True,
+        default=None)
+    max_height = models.FloatField(
+        max_length=10,
+        default='0'
+    )
+    min_height = models.FloatField(
+        max_length=10,
+        default='0'
+    )
+    max_weight = models.FloatField(
+        max_length=10,
+        default='0'
+    )
+    min_weight = models.FloatField(
+        max_length=10,
+        default='0'
+    )
+    egg_group_1 = models.CharField(
+        max_length=10,
+        choices=EGG_GROUPS,
+        default='Undiscovered'
+    )
+    egg_group_2 = models.CharField(
+        max_length=10,
+        choices=EGG_GROUPS,
+        blank=True,
+        null=True,
+        default=None
+    )
+    ability_1 = models.ForeignKey(
+        'Ability', 
+        models.SET_NULL,
+        related_name='ability_1',
+        blank=True,
+        null=True,
+    )
+    ability_2 = models.ForeignKey(
+        'Ability', 
+        models.SET_NULL,
+        related_name='ability_2', 
+        blank=True,
+        null=True,
+    )
+    hidden_ability = models.ForeignKey(
+        'Ability',  
+        models.SET_NULL,
+        related_name='hidden_ability',
+        blank=True,
+        null=True,
+    )
+
+
+    def __str__(self):
+        return self.species_name
+
+    def type(self):
+        bothTypes = self.get_type_primary_display()
+        if (self.type_secondary is not None):
+            bothTypes += "/" + self.get_type_secondary_display()
+        return bothTypes
+    
+    class Meta:
+        verbose_name_plural = 'Species'
+
+class Ability(models.Model):
+    ability_name = models.CharField('Ability Name', max_length=30)
+    ability_desc = models.CharField('Description', max_length=300)
+
+    def __str__(self):
+        return self.ability_name
+
+        
+    class Meta:
+        verbose_name_plural = 'Abilities'
